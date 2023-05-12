45eb614ae6f0827f2eb6ec98b69e422db11d30a7
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2023-05-13 01:24:23 +0800

    Модель Task: добавлены новые методы
    Добавлены методы для получения полей класса. Также реализован метод для получения данных из базы данных по id задачи.
    getNormalStatus и getNormalType - для красивого вывода в html

--- app/models/Task.php
+++ app/models/Task.php
@@ -39,6 +39,53 @@ class Task
         $this->user = $user;
     }
 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    // Getter для theme
+    public function getTheme()
+    {
+        return $this->theme;
+    }
+
+    // Getter для type
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    // Getter для location
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    // Getter для startDate
+    public function getStartDate()
+    {
+        return $this->start_date;
+    }
+
+    // Getter для duration
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+
+    // Getter для comment
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    // Getter для status
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
     public function create()
     {
         /*
@@ -82,11 +129,66 @@ class Task
         return $tasks;
     }
 
+    public function getById($id)
+    {
+        $query = "SELECT * FROM tasks WHERE id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 0) {
+            die('Задача не найдена');
+        }
+        $data = $result->fetch_assoc();
+        $this->id = $id;
+        $this->theme = $data['theme'];
+        $this->type = $data['type'];
+        $this->location = $data['location'];
+        $this->start_date = $data['start_date'];
+        $this->duration = $data['duration'];
+        $this->comment = $data['comment'];
+        $this->status = $data['status'];
+    }
+
+    public function getNormalStatus()
+    {
+        switch ($this->status) {
+            case  'in_work':
+                return 'Выполняется';
+                break;
+            case  'done':
+                return 'Выполнена';
+                break;
+            default:
+                return 'Не выполнена';
+                break;
+        }
+    }
+
+    public function getNormalType()
+    {
+        switch ($this->type) {
+            case  'meeting':
+                return 'Встреча';
+                break;
+            case  'call':
+                return 'Звонок';
+                break;
+            case  'conference':
+                return 'Совещание';
+                break;
+            default:
+                return 'Дело';
+                break;
+        }
+    }
+
     public function delete()
     {
         /*
-         * Удалить задачу
-         */
+        * Удалить задачу
+        */
         $stmt = $this->db->prepare('DELETE FROM tasks WHERE id = ? AND user_id = ?');
         $user_id = $this->user->getId();
         $stmt->bind_param('ii', $this->id, $user_id);
@@ -97,8 +199,8 @@ class Task
     public function update()
     {
         /*
-         * Обновление информации о задаче в базе данных
-         */
+        * Обновление информации о задаче в базе данных
+        */
         $stmt = $this->db->prepare('UPDATE tasks SET user_id = ?, theme = ?, type = ?, location = ?, start_date = ?, duration = ?, comment = ?, status = ? WHERE id = ?');
         $user_id = $this->user->getId();
         $stmt->bind_param('issssisii', $user_id, $this->theme, $this->type, $this->location, $this->start_date, $this->duration, $this->comment, $this->status, $this->id);
